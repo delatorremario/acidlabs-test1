a07873857d58bc52ba8e9a92d921a789f7819f6b
Mario de la Torre <delatorremario@gmail.com>
2017-04-07 19:41:38 -0300

    init hapi server

---
+++ server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/name/{name}', 
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+//Statics Pages
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/helloworld',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:',server.info.uri);
+});
+
+
